d96513600c009584c7952a8886d4262af75ce46c
E. Bamba <ebamba@bitsnshine.com>
2018-09-26 12:41:54 -0400

    Service worker development

--- sw.js
+++ sw.js
@@ -3,13 +3,13 @@ const cacheName = 'v1';
  * Install the service worker
  */
 const cachedElements = [
-    '/index.html',
-    '/restaurant.html',
-    '/assets/main.js',
-    '/assets/styles.css',
-    '/assets/css/webfonts.scss',
-    '/restaurant.html',
+    'index.html',
+    'restaurant.html',
+    '/assets/js/main.js',
+    '/assets/css/styles.css',
+    '/assets/css/webfonts.css',
     '/assets/js/dbhelper.js',
+    '/assets/js/restaurant_info.js',
     '/assets/img/1.jpg',
     '/assets/img/2.jpg',
     '/assets/img/3.jpg',
@@ -21,23 +21,41 @@ const cachedElements = [
     '/assets/img/9.jpg',
     '/assets/img/10.jpg'
 ];
-self.addEventListener('install', function(e) {
+self.addEventListener('install', function (ev) {
     console.log('Service Worker installed');
     // Handle the cached objects
-    e.waitUntil(
-    caches
-    .open(cacheName)
-    .then (cache => {
-        console.log('Service worker installed');
-        cache.addAll(cachedElements)
-    .then(() =>
-        self.skipWaiting())
-    }));
+    ev.waitUntil(
+        caches
+        .open(cacheName)
+        .then(cache => {
+            console.log('Caching assets');
+            cache.addAll(cachedElements)
+                .then(() => self.skipWaiting());
+        }));
 });
 
 /**
  * Activate service worker
  */
-self.addEventListener('activate', function(e) {
-    console.log('Service worker activated');
+self.addEventListener('activate', function (ev) {
+
+    // Removed unwanted caches
+    ev.waitUntil(
+        caches.keys()
+        .then(cacheName => {
+            return Promise.map(cache => {
+                if (cache !== cacheName) {
+                    // Delete all other caches that we are not dealing with at the moment
+                    return caches.delete(cache);
+                }
+            });
+        })
+    );
+});
+// Show offline files
+self.addEventListener('fetch', ev => {
+    // check if there is network or not
+    
+    ev.respondWith(
+        fetch(ev.request).catch(() => caches.match(ev.request)));
 });
